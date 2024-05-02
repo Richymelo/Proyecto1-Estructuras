61581b56abd63f6351f54af1784269a427c29756
Richymelo <castroricardojimenez@gmail.com>
2024-05-01 18:32:31 -0600

    Tiempo de carga

--- Estructuras-Proyecto1B/Estructuras-Proyecto1B/Estructuras-Proyecto1B.cpp
+++ Estructuras-Proyecto1B/Estructuras-Proyecto1B/Estructuras-Proyecto1B.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <ctime> 
 
 using namespace std;
 
@@ -129,4 +130,15 @@ void CargarPadron(PtrPersona& Lista) {
         }
     }
     fclose(archivo);
+}
+void main() {
+    unsigned t0, t1;
+
+    t0 = clock();
+    // Code to execute
+    t1 = clock();
+
+    double time = (double(t1 - t0) / CLOCKS_PER_SEC);
+    cout << "Execution Time: " << time << endl;
+}
 
